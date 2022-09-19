6a87fcb9755bb2177bf3727af2552941ee6899bf
Bernard <cesarzb@protonmail.com>
2022-09-19 16:33:43 +0200

    Added validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true
+    validates :username, presence: true, length: { minimum: 3, maximum: 16 }
+    validates :password, length: { minimum: 8, maximum: 32 }
 
     def token_valid?
         self.token_expiration > Time.now ? true : false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-#  pending "add some examples to (or delete) #{__FILE__}"
+    describe 'username' do
+        let(:user) { FactoryBot.build(:user) }
+
+        it 'is not valid when blank' do
+            user.username = ' '
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+        it 'is not valid when shorter than 3 letters' do
+            user.username = 'aa'
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+        it 'is not valid when longer than 16 letters' do
+            user.username = 'a' * 17
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+    end
+
+    describe 'password' do
+        let(:user) { FactoryBot.build(:user) }
+
+        it 'is not valid when blank' do
+            user.password = ' '
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+        it 'is not valid when shorter than 8 letters' do
+            user.password = 'a' * 7
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+        it 'is not valid when longer than 32 letters' do
+            user.password = 'a' * 33
+            user.save
+
+            expect(user).not_to be_valid
+        end
+
+    end
 end
